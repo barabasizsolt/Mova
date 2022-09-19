a1e79d3b18ea21ef60a5cd0b8216a23eca20d1db
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2022-09-19 14:30:14 +0300

    Improved service module

--- service/model/people/src/main/java/com/barabasizsolt/people/model/People.kt
+++
@@ -1,8 +0,0 @@
-package com.barabasizsolt.people.model
-
-data class People(
-    val id: String,
-    val adult: Boolean,
-    val profilePath: String,
-    val name: String
-)
--- service/model/people/src/main/java/com/barabasizsolt/people/model/PeopleList.kt
+++
@@ -1,6 +0,0 @@
-package com.barabasizsolt.people.model
-
-data class PeopleList(
-    val page: Int,
-    val results: List<People>
-)
