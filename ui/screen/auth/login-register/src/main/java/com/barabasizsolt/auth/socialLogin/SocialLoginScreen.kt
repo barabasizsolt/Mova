f8f02cda62c30ab5d37d8863669f4238bbd87faf
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2022-11-06 19:51:33 +0200

    improved socialLoginScreen state handling & removed screenState from AuthScreen's ScreenContent function

--- ui/screen/auth/login-register/src/main/java/com/barabasizsolt/auth/socialLogin/SocialLoginScreen.kt
+++ ui/screen/auth/login-register/src/main/java/com/barabasizsolt/auth/socialLogin/SocialLoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.SnackbarHostState
@@ -57,7 +58,7 @@ fun SocialLoginScreen(screenState: SocialLoginScreenState) {
                 snackBarHostState.currentSnackbarData?.dismiss()
                 screenState.resetState()
             },
-            modifier = Modifier.align(alignment = Alignment.BottomCenter)
+            modifier = Modifier.systemBarsPadding()
         )
     }
 
