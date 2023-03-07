f6e1c0fe6bf64a10f0c7f7a7dc04cb4ed4e34fd7
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-03-07 09:36:34 +0200

    Replace 'merge' with 'combine' inside GetGenresFlowUseCase.

--- domain/domain-usecase/src/main/java/com/barabasizsolt/domain/usecase/helper/genre/GetGenresFlowUseCase.kt
+++ domain/domain-usecase/src/main/java/com/barabasizsolt/domain/usecase/helper/genre/GetGenresFlowUseCase.kt
@@ -2,14 +2,16 @@ package com.barabasizsolt.domain.usecase.helper.genre
 
 import com.barabasizsolt.genre.api.GenreService
 import com.barabasizsolt.genre.api.GenreType
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.merge
 
 class GetGenresFlowUseCase(private val genreService: GenreService) {
 
     operator fun invoke(genreType: GenreType = GenreType.JOINT) = when (genreType) {
         GenreType.MOVIE -> genreService.movieGenres.filterNotNull()
         GenreType.TV -> genreService.tvGenres.filterNotNull()
-        GenreType.JOINT -> merge(genreService.movieGenres, genreService.tvGenres).filterNotNull()
+        GenreType.JOINT -> genreService.movieGenres.combine(genreService.tvGenres) { movieGenres, tvGenres ->
+            movieGenres + tvGenres
+        }
     }
 }
