5081f018ae7580dfe27ccc2d0427a1896401f93d
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-03-22 17:46:45 +0200

    Temporary implementation of DetailScreen & DetailScreenState

--- ui/screen/main/detail/src/main/java/com/barabasizsolt/detail/DetailScreen.kt
+++ ui/screen/main/detail/src/main/java/com/barabasizsolt/detail/DetailScreen.kt
@@ -1,19 +1,143 @@
 package com.barabasizsolt.detail
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridItemSpanScope
+import androidx.compose.foundation.lazy.grid.LazyGridState
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.barabasizsolt.base.BaseScreen
+import com.barabasizsolt.catalog.WatchableWithRating
+import com.barabasizsolt.detail.catalog.CastCrewContent
+import com.barabasizsolt.detail.catalog.ContentHeader
+import com.barabasizsolt.detail.catalog.ContentTabs
+import com.barabasizsolt.domain.model.ContentItem
+import com.barabasizsolt.domain.model.DetailScreenContent
+import com.barabasizsolt.domain.model.toContentItem
 import com.barabasizsolt.theme.AppTheme
+import com.barabasizsolt.util.YoutubePlayer
+import com.barabasizsolt.util.navigationBarInsetDp
 
 @Composable
 fun DetailScreen(screenState: DetailScreenState) {
-    Box {
-        Text(
-            text = "Detail",
-            style = AppTheme.typography.h6,
-            modifier = Modifier.align(alignment = Alignment.Center)
-        )
+    var tabIndex by remember { mutableStateOf(value = 0) }
+
+    BaseScreen(
+        screenState = screenState,
+        content = { gridState, _ ->
+            ScreenContent(
+                item = screenState.details,
+                gridState = gridState,
+                tabIndex = tabIndex,
+                onTabIndexChange = { index -> tabIndex = index }
+            )
+        }
+    )
+}
+
+/*TODO: handle empty videos, movies, comments*/
+@Composable
+private fun ScreenContent(
+    item: DetailScreenContent,
+    gridState: LazyGridState,
+    tabIndex: Int,
+    onTabIndexChange: (Int) -> Unit
+) = LazyVerticalGrid(
+    columns = GridCells.Fixed(count = 2),
+    modifier = Modifier.fillMaxSize(),
+    state = gridState,
+    verticalArrangement = Arrangement.spacedBy(space = AppTheme.dimens.contentPadding * 2),
+    horizontalArrangement = Arrangement.spacedBy(space = AppTheme.dimens.contentPadding),
+    contentPadding = PaddingValues(bottom = navigationBarInsetDp),
+    content = {
+        item(span = itemSpan) {
+            ContentHeader(
+                item = item,
+                onPlayButtonClicked = { /*TODO: Implement it*/ },
+                onAddToFavouriteButtonClicked = { /*TODO: Implement it*/ }
+            )
+        }
+        item(span = itemSpan) {
+            CastCrewContent(
+                castCrew = (item as DetailScreenContent.MovieDetails).castCrew,
+                onItemClick = { /*TODO: Implement it*/ }
+            )
+        }
+        item(span = itemSpan) {
+            ContentTabs(
+                tabIndex = tabIndex,
+                onTabIndexChange = onTabIndexChange
+            )
+        }
+        when (tabIndex) {
+            0 -> itemsIndexed(
+                items = (item as DetailScreenContent.MovieDetails).videos,
+                span = getTabItemsSpan(tabIndex = tabIndex),
+                key = { index, item -> "${index}-${item.id}" }
+            ) { _, item ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = AppTheme.dimens.screenPadding)
+                ) {
+                    Text(
+                        text = item.name,
+                        style = AppTheme.typography.body1,
+                        fontWeight = FontWeight.Bold
+                    )
+                    YoutubePlayer(videoId = item.key)
+                }
+            }
+            1 -> itemsIndexed(
+                items = (item as DetailScreenContent.MovieDetails).similarMovies,
+                span = getTabItemsSpan(tabIndex = tabIndex)
+            ) { index, item ->
+                WatchableWithRating(
+                    item = item.toContentItem() as ContentItem.Watchable,
+                    onClick = { /*TODO: Implement it*/ },
+                    modifier = Modifier.padding(
+                        start = if (index % 2 == 0) AppTheme.dimens.screenPadding else 0.dp,
+                        end = if (index % 2 == 0) 0.dp else AppTheme.dimens.screenPadding,
+                    )
+                )
+            }
+            2 -> itemsIndexed(
+                items = (item as DetailScreenContent.MovieDetails).reviews,
+                span = getTabItemsSpan(tabIndex = tabIndex)
+            ) { _, item ->
+                /*TODO: Implement it*/
+                Text(
+                    text = item.content,
+                    style = AppTheme.typography.body1,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
+)
+
+private val itemSpan: (LazyGridItemSpanScope.() -> GridItemSpan) = { GridItemSpan(currentLineSpan = 2) }
+
+private fun getTabItemsSpan(tabIndex: Int): (LazyGridItemSpanScope.(index: Int, item: Any) -> GridItemSpan) = { _, _ ->
+    GridItemSpan(
+        currentLineSpan = when (tabIndex) {
+            0, 2 -> 2
+            else -> 1
+        }
+    )
 }
--- ui/screen/main/detail/src/main/java/com/barabasizsolt/detail/DetailScreenState.kt
+++ ui/screen/main/detail/src/main/java/com/barabasizsolt/detail/DetailScreenState.kt
@@ -1,41 +1,55 @@
 package com.barabasizsolt.detail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import com.barabasizsolt.base.BaseScreenState
+import com.barabasizsolt.base.UserAction
+import com.barabasizsolt.domain.model.DetailScreenContent
 import com.barabasizsolt.domain.usecase.screen.detail.GetMovieDetailsUseCase
 import com.barabasizsolt.domain.util.result.Result
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.get
 
 @Composable
 fun rememberDetailScreenState(
     id: Int,
-    scope: CoroutineScope = rememberCoroutineScope(),
     getMovieDetailsUseCase: GetMovieDetailsUseCase = get()
 ) = remember {
     DetailScreenState(
         id = id,
-        scope = scope,
         getMovieDetailsUseCase = getMovieDetailsUseCase
     )
 }
 
 class DetailScreenState(
     val id: Int,
-    private val scope: CoroutineScope,
     private val getMovieDetailsUseCase: GetMovieDetailsUseCase
-) {
+) : BaseScreenState() {
+
+    var details by mutableStateOf<DetailScreenContent>(value = DetailScreenContent.MovieDetails.createEmptyMovieDetailContent())
+        private set
 
     init {
+        getScreenData(userAction = UserAction.Normal)
+    }
+
+    override fun getScreenData(userAction: UserAction, delay: Long) {
+        state = when (userAction) {
+            UserAction.SwipeRefresh -> State.SwipeRefresh
+            else -> State.Loading
+        }
         scope.launch {
-            when (val result = getMovieDetailsUseCase(id = id)) {
+            state = when (val result = getMovieDetailsUseCase(id = id)) {
                 is Result.Failure -> {
-                    println("<<ERROR: ${result.exception.message}")
+                    State.Error(message = result.exception.message.toString())
                 }
                 is Result.Success -> {
                     println("<<SUCCESS: ${result.data.id}")
+                    details = result.data
+                    State.Normal
                 }
             }
         }
