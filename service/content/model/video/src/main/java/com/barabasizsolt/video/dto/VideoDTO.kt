18f928fc80a709ff40f23d3a24925089dee0c4f5
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-03-23 18:14:55 +0200

    Rename Video's key to videoId.

--- service/content/model/video/src/main/java/com/barabasizsolt/video/dto/VideoDTO.kt
+++ service/content/model/video/src/main/java/com/barabasizsolt/video/dto/VideoDTO.kt
@@ -24,7 +24,7 @@ fun VideoDTO.toModel() : Video? {
     return Video(
         id = id,
         name = name,
-        key = key,
+        videoId = key,
         site = site
     )
 }
--- service/content/model/video/src/main/java/com/barabasizsolt/video/model/Video.kt
+++ service/content/model/video/src/main/java/com/barabasizsolt/video/model/Video.kt
@@ -3,6 +3,6 @@ package com.barabasizsolt.video.model
 data class Video(
     val id: String,
     val name: String,
-    val key: String,
+    val videoId: String,
     val site: String
 )
