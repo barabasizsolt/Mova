ac9b6737b529ccc7682da85a8001ee0c4621d4a6
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-03-23 18:14:37 +0200

    Add lifecycleOwner to YouTubePlayer.

--- ui/util/src/main/java/com/barabasizsolt/util/Youtube.kt
+++ ui/util/src/main/java/com/barabasizsolt/util/Youtube.kt
@@ -3,6 +3,7 @@ package com.barabasizsolt.util
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerFullScreenListener
@@ -13,12 +14,12 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.DefaultPlayerUiCo
 @Composable
 fun YoutubePlayer(
     videoId: String,
+    lifecycleOwner: LifecycleOwner,
     modifier: Modifier = Modifier
 ) {
     AndroidView(
         factory = { context ->
             val view = YouTubePlayerView(context)
-
             val options: IFramePlayerOptions = IFramePlayerOptions.Builder().controls(0).build()
 
             val listener = object : AbstractYouTubePlayerListener() {
@@ -35,6 +36,8 @@ fun YoutubePlayer(
                 playerOptions = options
             )
 
+            lifecycleOwner.lifecycle.addObserver(view)
+
             view
         },
         update = { view ->
@@ -42,7 +45,6 @@ fun YoutubePlayer(
             view.addFullScreenListener(
                 fullScreenListener = object : YouTubePlayerFullScreenListener {
                     override fun onYouTubePlayerEnterFullScreen() {
-                        println("<<HERE")
                         view.enterFullScreen()
                     }
 
