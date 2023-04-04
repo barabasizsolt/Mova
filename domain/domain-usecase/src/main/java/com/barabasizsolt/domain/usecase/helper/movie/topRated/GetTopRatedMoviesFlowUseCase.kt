865057c851d76f44466972a7d7b00bc44c21ef42
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-04-04 12:29:34 +0300

    Filter out the unique elements of every movie/tv/people related use case.

--- domain/domain-usecase/src/main/java/com/barabasizsolt/domain/usecase/helper/movie/topRated/GetTopRatedMoviesFlowUseCase.kt
+++ domain/domain-usecase/src/main/java/com/barabasizsolt/domain/usecase/helper/movie/topRated/GetTopRatedMoviesFlowUseCase.kt
@@ -2,8 +2,9 @@ package com.barabasizsolt.domain.usecase.helper.movie.topRated
 
 import com.barabasizsolt.movie.api.MovieService
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class GetTopRatedMoviesFlowUseCase(private val movieService: MovieService) {
 
-    operator fun invoke() = movieService.topRatedMovies.filterNotNull()
+    operator fun invoke() = movieService.topRatedMovies.filterNotNull().map { it.distinctBy { movie -> movie.id } }
 }
