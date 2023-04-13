f4c9ad753aa68d8e8430778bd29f384df6b50d07
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-04-13 11:32:59 +0300

    Migrate app's top level gradle module from Groovy to Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+}
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.github.ben.manes.versions) apply false
+    alias(libs.plugins.littlerobots) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
