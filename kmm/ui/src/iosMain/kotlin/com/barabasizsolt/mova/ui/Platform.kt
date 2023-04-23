928f9f992fc4f9a4c3b14416fb6c89ee198d15a5
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-04-24 02:44:56 +0300

    Improve navigationBarInset & statusBarInset for IOS.

--- kmm/ui/src/iosMain/kotlin/com/barabasizsolt/mova/ui/Platform.kt
+++ kmm/ui/src/iosMain/kotlin/com/barabasizsolt/mova/ui/Platform.kt
@@ -3,21 +3,29 @@ package com.barabasizsolt.mova.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.barabasizsolt.mova.ui.theme.AppTheme
+import com.barabasizsolt.mova.ui.util.isXFamilyDevice
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import platform.CoreGraphics.CGRectGetHeight
 import platform.UIKit.UIDevice
+import platform.UIKit.UIScreen
 
 class IOSPlatform: Platform {
 
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 
+    private val isXFamilyIPhone: Boolean = UIScreen.mainScreen.bounds.let { bounds ->
+        CGRectGetHeight(bounds)
+    }.isXFamilyDevice() && UIDevice.currentDevice.model.startsWith(prefix = "iPhone")
+
     override val navigationBarInsetDp: Dp
         @Composable
-        get() = 0.dp
+        get() = if (isXFamilyIPhone) AppTheme.dimens.screenPadding else 0.dp
 
     override val statusBarInsetDp: Dp
         @Composable
-        get() = 0.dp
+        get() = if (isXFamilyIPhone) AppTheme.dimens.screenPadding * 2 else AppTheme.dimens.contentPadding
 
     override val imeBottomInsetDp: Dp
         @Composable
