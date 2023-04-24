7291afed148eecf32a0020a215539c612f5e992b
Zsolt Barabási <barabasi.robert.zsolt@gmail.com>
2023-04-24 14:55:04 +0300

    Update main.ios.kt

--- kmm/shared/src/iosMain/kotlin/main.ios.kt
+++ kmm/shared/src/iosMain/kotlin/main.ios.kt
@@ -1,11 +1,12 @@
 package com.barabasizsolt.mova.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.barabasizsolt.mova.ui.navigation.appNav.AppNavigation
-import com.barabasizsolt.mova.ui.theme.MovaTheme
+import ui.navigation.appNav.AppNavigation
+import ui.theme.MovaTheme
 
 fun MainViewController() = ComposeUIViewController {
     MovaTheme {
         AppNavigation()
+        //HomeScreen(screenState = rememberHomeScreenState())
     }
 }
